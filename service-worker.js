294955037839fd1949a33782e4f4615ad400f5b5
javidei <javyspot@gmail.com>
2026-08-08 11:38:08 +0200

    chore: refrescar caché cvitae v0.3.6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PORTFOLIO_VERSION = "0.3.5";
+const PORTFOLIO_VERSION = "0.3.6";
 const CACHE_PREFIX = "cvitae-shell-";
 const CACHE_NAME = `${CACHE_PREFIX}${PORTFOLIO_VERSION}-standalone-projects`;
 
