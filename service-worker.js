fc2c503cc9d56781876599d80c7c0e80c06cfd2c
javidei <javyspot@gmail.com>
2026-08-09 01:58:56 +0200

    Actualiza caché para orden dinámico de proyectos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PORTFOLIO_VERSION = "0.3.12";
+const PORTFOLIO_VERSION = "0.3.13";
 const CACHE_PREFIX = "cvitae-shell-";
 const CACHE_NAME = `${CACHE_PREFIX}${PORTFOLIO_VERSION}-standalone-projects`;
 
