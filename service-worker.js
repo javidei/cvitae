72e59dbb94ece32f32c30101b653b54f99e3d22e
javidei <javyspot@gmail.com>
2026-08-09 23:19:18 +0200

    Renueva la caché tras eliminar el splash móvil

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PORTFOLIO_VERSION = "0.3.15";
+const PORTFOLIO_VERSION = "0.3.16";
 const CACHE_PREFIX = "cvitae-shell-";
 const CACHE_NAME = `${CACHE_PREFIX}${PORTFOLIO_VERSION}-standalone-projects`;
 
