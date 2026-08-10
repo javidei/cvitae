b6ae53e10300dfa345eaab2d878abbbb81c7733e
javidei <javyspot@gmail.com>
2026-08-10 11:00:44 +0200

    Renueva la caché de cvitae 0.3.20

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PORTFOLIO_VERSION = "0.3.19";
+const PORTFOLIO_VERSION = "0.3.20";
 const CACHE_PREFIX = "cvitae-shell-";
 const CACHE_NAME = `${CACHE_PREFIX}${PORTFOLIO_VERSION}-standalone-projects`;
 
